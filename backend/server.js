9be1ba93bc0c04ae2db097a35d36c4376fc26415
Web Developer <developer@example.com>
2026-02-27 16:34:08 +0530

    commit 22

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = 3000;
 
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(cors());
 
